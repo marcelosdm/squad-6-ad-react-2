585bfce0434abc8862e346a29c3aed0281854d22
xavierleonardo <gxavier.leonardo@gmail.com>
2019-07-05 02:25:50 -0300

    add first smoke tests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Header () {
-  return <header>Header</header>
+  return (
+    <header>
+      <img src='' />
+      GitHub React
+      <input type='text' placeholder='' />
+    </header>
+  )
 }
 
 export default Header
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,12 +1,29 @@
+import 'jest-dom/extend-expect'
 import React from 'react'
-import { render } from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
 import Header from './Header'
+
+afterEach(cleanup)
 
 describe('Header', () => {
   it('should exist', () => {
     const { container } = render(<Header />)
-    const app = container.getElementsByTagName('header')
+    const [header] = container.getElementsByTagName('header')
 
-    expect(app).toBeDefined()
+    expect(header).toBeInTheDocument()
+  })
+
+  it('should have a logo image', () => {
+    const { container } = render(<Header />)
+    const [logo] = container.getElementsByTagName('img')
+
+    expect(logo).toBeInTheDocument()
+  })
+
+  it('should have an input to search for users', () => {
+    const { container } = render(<Header />)
+    const [input] = container.getElementsByTagName('input')
+
+    expect(input).toBeInTheDocument()
   })
 })
