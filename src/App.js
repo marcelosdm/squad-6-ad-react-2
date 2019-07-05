0b688e35366b4904d319012b19bb9834dbdefc10
xavierleonardo <gxavier.leonardo@gmail.com>
2019-07-05 00:28:18 -0300

    add teste api query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { graphql } from 'react-apollo'
+
+const testQuery = gql`
+  query {
+    viewer {
+      login
+    }
+  }
+`
 
 function App () {
   return <div className='App'>GitHub React</div>
 }
 
-export default App
+export default graphql(testQuery)(App)
